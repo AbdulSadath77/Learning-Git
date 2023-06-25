64db786f0a52538950383a37b16bf3b3da69091e
sadath <sadathsd69@gmail.com>
2023-06-25 18:08:21 +0530

    script file added to index and index is updated

---
+++ script.js
@@ -0,0 +1,5 @@
+let edit_btn = document.getElementById("button");
+edit_btn.addEventListener("click", () => {
+  let des = document.getElementById("description");
+  des.style.backgroundColor = "green";
+});
